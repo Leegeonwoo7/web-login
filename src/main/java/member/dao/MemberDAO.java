6d515b4aea4c2ee9768e6e29512cf01ff088ef31
Leegeonwoo7 <workgeon07@naver.com>
2024-02-04 01:44:14 +0900

    회원가입 정보처리 / 로그인 처리

--- src/main/java/member/dao/MemberDAO.java
+++ src/main/java/member/dao/MemberDAO.java
@@ -1,5 +1,8 @@
 package member.dao;
 
+import member.bean.MemberDTO;
+
+import java.lang.reflect.Member;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -16,20 +19,22 @@ public class MemberDAO {
     private String url = "jdbc:mysql://localhost:3306/world";
     private String username = "root";
     private String password = "1234";
-//world?serverTimezone=UTC
+
+    //world?serverTimezone=UTC
     //싱글톤패턴
-    public static MemberDAO getInstance(){
+    public static MemberDAO getInstance() {
         return memberDAO;
     }
-    public MemberDAO(){
-        try{
+
+    public MemberDAO() {
+        try {
             Class.forName(driver);
-        }catch(ClassNotFoundException e){
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public void getConnection(){
+    public void getConnection() {
         try {
             conn = DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
@@ -37,8 +42,8 @@ public class MemberDAO {
         }
     }
 
-    public boolean isExistId(String id){
-        boolean exist = false;
+    public boolean isExistId(String id) {
+        boolean exist = false; //false이면 중복없음
         String sql = "select * from member where id = ?";
 
         getConnection();
@@ -47,20 +52,87 @@ public class MemberDAO {
             pstmt.setString(1, id);
             rs = pstmt.executeQuery(); //실행-ResultSet 리턴
 
-            if(rs.next()) exist = true; //레코드가 존재하기때문에 사용이 불가능
+            if (rs.next()) exist = true; //레코드가 존재하기때문에 사용이 불가능
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             try {
-                if(rs != null) rs.close();
-                if(pstmt != null) pstmt.close();
-                if(conn != null) conn.close();
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         }
-
         return exist;
     }
+
+    public boolean write(MemberDTO memberDTO) {
+        String sql = "insert into member values(?,?,?,?,?,?,?,?,?,?,?,?,sysdate)";
+
+        getConnection();
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, memberDTO.getName());
+            pstmt.setString(2, memberDTO.getId());
+            pstmt.setString(3, memberDTO.getGender());
+            pstmt.setString(4, memberDTO.getEmail1());
+            pstmt.setString(5, memberDTO.getEmail2());
+            pstmt.setString(6, memberDTO.getEmail3());
+            pstmt.setString(7, memberDTO.getTel1());
+            pstmt.setString(8, memberDTO.getTel2());
+            pstmt.setString(9, memberDTO.getTel3());
+            pstmt.setString(10, memberDTO.getZipcode());
+            pstmt.setString(11, memberDTO.getAddr1());
+            pstmt.setString(12, memberDTO.getAddr2());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
+    }
+
+    public MemberDTO login(String id, String pwd) {
+        MemberDTO memberDTO = null;
+        String sql = "SELECT name, id, email, email2, FROM member WHERE id = ? AND pwd = ?";
+
+        getConnection();
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, id);
+            pstmt.setString(2, pwd);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                memberDTO = new MemberDTO();
+                memberDTO.setName(rs.getString("name"));
+                memberDTO.setId(rs.getString("id"));
+                memberDTO.setEmail1(rs.getString("email1"));
+                memberDTO.setEmail1(rs.getString("email2"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+                ;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return memberDTO;
+    }
 }
+
+
 //jar파일넣기 WEB-INF -> lib에 넣기
